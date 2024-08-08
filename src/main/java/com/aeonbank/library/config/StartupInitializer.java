cc8f8a8ac32a624494b6cd8d0d3c3d0315a879d0
Cyrus Wong Yong Lin <cyrus.wong@up-devlabs.io>
2024-08-08 17:32:10 +0800

    display branch name and commit id during app startup

--- src/main/java/com/aeonbank/library/config/StartupInitializer.java
+++ src/main/java/com/aeonbank/library/config/StartupInitializer.java
@@ -4,17 +4,25 @@ import com.aeonbank.library.repository.BookRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class StartupInitializer {
 
+    @Value("${my.branch-name}")
+    private String branchName;
+
+    @Value("${my.commit-id}")
+    private String commitId;
+
     @Autowired
     private BookRepository bookRepository;
 
     @PostConstruct
     public void init() {
+        displayAppInfo();
         initFirstDbRequest();
     }
 
@@ -25,6 +33,10 @@ public class StartupInitializer {
         } catch (Exception e) {
             log.error("[initFirstDbRequest]database connection initialization failed. error >>> ", e);
         }
+    }
+
+    private void displayAppInfo(){
+        log.info("[displayAppInfo]app is running on branch:{} with commitId:{}", branchName, commitId);
     }
 
 }
